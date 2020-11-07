dacb84f8c4db6ce187ab713c82b88046403b23b5
“briana <briana@thesistesging.com>
2020-11-07 16:42:41 -0500

    update code

--- app/js/global.js
+++ app/js/global.js
@@ -1,3 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log('helloooooo')
+$( document ).ready(function() {
+    $('.filters-list a').on('click', function() {
+      var filter = $(this).attr('data-filter')
+      
+      $('.contact').hide()
+      $('.photo').hide()
+      $(filter).show()
+      
+    $('.filters-list a').removeClass('selected')
+      
+    $(this).addClass('selected')
+
+    return false 
+      
+    })
+
+    $(document).on('scroll', function() {
+      var pixelsFromTop = $(document).scrollTop()
+
+      if (pixelsFromTop > 50) {
+        $('header').addClass('hidden')
+      } else {
+        $('header').removeClass('hidden')
+      }
+
+      var documentHeight = $(document).height()
+      var windowHeight = $(window).height()
+
+      var difference = documentHeight - windowHeight
+      var percentage = 100 * pixelsFromTop / difference
+
+      console.log(percentage)
+
+      $('.bar').css('width', percentage + '%')
+    })
+
+
 });
